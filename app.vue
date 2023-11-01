f90ef8aae4556e5dacaa689203b8331c382d1d87
nijarr_ali <87968939+nijaare@users.noreply.github.com>
2023-11-02 05:47:28 +0700

    Update app.vue

--- app.vue
+++ app.vue
@@ -1,21 +1,12 @@
 <script setup lang="ts">
-import { useWindowSize } from 'vue-window-size';
-const { width } = useWindowSize();
-
-const panjangLayar = ref(width)
 
 </script>
 
 <template>
   <div class="mobile-layout">
-    <div class="" v-if="panjangLayar <= 550">
       <NuxtLayout>
         <NuxtPage />
       </NuxtLayout>
-    </div>
-    <div v-else class="flex justify-center items-center h-screen">
-      <p class="text-center">Website ini hanya mendukung untuk layar mobile, Kecilkan lebar layar jika ingin mengakses website</p>
-    </div>
   </div>
 </template>
 
